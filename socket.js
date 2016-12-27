68affa3b113c7c64d3a14179ef6a8cc8fc336596
delvedor <tommydelved@gmail.com>
2016-12-27 12:26:58 +0100

    First code commit

---
+++ socket.js
@@ -0,0 +1,69 @@
+'use strict'
+
+function Socket (uri) {
+  if (!(this instanceof Socket)) {
+    return new Socket(uri)
+  }
+
+  this.listeners = {}
+  this.socket = new window.WebSocket(uri)
+
+  this.socket.onmessage = (event) => {
+    var data = toJSON(event.data)
+    if (data) {
+      if (this.listeners[data.event]) {
+        this.listeners[data.event](data)
+      }
+    } else {
+      if (this.listeners['message']) {
+        this.listeners['message'](event.data)
+      }
+    }
+  }
+
+  this.socket.onopen = (event) => {
+    if (this.listeners['open']) {
+      this.listeners['open'](event)
+    }
+  }
+
+  this.socket.onerror = (event) => {
+    if (this.listeners['error']) {
+      this.listeners['error'](event)
+    }
+  }
+
+  this.socket.onclose = (event) => {
+    if (this.listeners['close']) {
+      this.listeners['close'](event)
+    }
+  }
+}
+
+Socket.prototype.on = function (event, callback) {
+  this.listeners[event] = callback
+}
+
+Socket.prototype.off = function (event, callback) {
+  if (this.listeners[event]) {
+    delete this.listeners[event]
+  }
+}
+
+Socket.prototype.close = function () {
+  this.socket.close()
+}
+
+Socket.prototype.send = function (data) {
+  this.socket.send(data)
+}
+
+function toJSON (data) {
+  try {
+    return JSON.parse(data)
+  } catch (e) {
+    return undefined
+  }
+}
+
+module.exports = Socket
